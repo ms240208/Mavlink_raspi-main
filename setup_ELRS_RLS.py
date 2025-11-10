eb88c447a1181842c37fc514c72b605bdc23f003
ms240208 <ms240208@g.u-fukui.ac.jps>
2025-11-10 18:53:39 +0900

    そこそこ揺れる

--- setup_ELRS_RLS.py
+++ setup_ELRS_RLS.py
@@ -87,7 +87,7 @@ params_to_set = {
     
     # --- 垂直制御PID ---
     'PSC_POSZ_P': 1,       # 高度位置制御P
-    'PSC_VELZ_P': 4.0,       # 垂直速度制御P
+    'PSC_VELZ_P': 4.00,       # 垂直速度制御P
     'PSC_VELZ_I': 8.0,       # 垂直速度制御I
     'PSC_VELZ_D': 0.01,      # 垂直速度制御D
     'PSC_ACCZ_P': 0.3,       # 垂直加速度制御P
